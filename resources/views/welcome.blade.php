b8d426447746a61a9b40c3244938364a13c9c78e
Łukasz Socha <lukasz.socha@cloudsofthouse.com>
2018-03-23 12:29:40 +0100

    welcome.blade merge with branch experiments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel React application</title>
-        <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <nav class="navbar navbar-light bg-light">
-          <h2 style="text-align: center"> Laravel and React application </h2>
-        </nav>
-        <div id="root"></div>
-        <script src="{{mix('js/app.js')}}" ></script>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h2 style="text-align: center"> Laravel and React application </h2>
+    <div id="root"></div>
+@endsection
